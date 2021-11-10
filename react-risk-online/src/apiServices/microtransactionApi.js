567c57bcd69bfe4ed76dcf54c9a3561898fa88a7
Soularflare <grfld93@yahoo.com>
2021-11-10 10:23:47 -0600

    microtransaction api

--- react-risk-online/src/apiServices/microtransactionApi.js
+++ react-risk-online/src/apiServices/microtransactionApi.js
@@ -1,11 +1,41 @@
-export async function findAvatars(){
-
+export async function findAvatars(userId){
+    const response = await fetch(url + "/appuser/");
+    if(response.status === 200) {
+        return response.json();
+    }
+    throw new Error("Unable to fetch avatars");
 };
 
-export async function findAvatarById(id){
 
+
+export async function addAvatar(id, userId){
+    const init = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        
+    }
+    const response = await fetch(url + "/appuser/" + id + "/" + userId, init);
+    if(response.status === 204) {
+        return;
+    }
+    throw new Error("Unable to add avatar");
 };
 
-export async function addAvatar(){
-
+export async function equipAvatar(id, userId){
+    const init = {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        
+    }
+    const response = await fetch(url + "/appuser/" + id + "/" + userId, init);
+    if(response.status === 204) {
+        return;
+    }
+    throw new Error("Unable to equip avatar");
 };
